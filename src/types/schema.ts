7496f7a2bad5ea71aaf5ba7b66305b1110182d29
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-23 14:44:01 +0530

    created schema type using zod

---
+++ src/types/schema.ts
@@ -0,0 +1,13 @@
+
+import { z } from 'zod';
+import { patterns } from '../constants';
+
+const schema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  email: z.string().min(1, { message: 'Email is required' })
+  .refine( email => patterns.email.test(email), {
+    message: 'Invalid email address'
+  }),
+});
+
+export type Schema = z.infer<typeof schema>;
