3da02e20cb953e440f7481825dbd17bd547e0fca
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-25 21:32:20 +0530

    created registrationDateAndTime field in schema

--- src/Users/types/schema.ts
+++ src/Users/types/schema.ts
@@ -13,6 +13,7 @@ export const schema = z.object({
   languagesSpoken: z.array(z.string()),
   gender: z.string().min(1),
   skills: z.array(z.string()).max(2, { message: "You can only select up to 2 skills" }),
+  registrationDateAndTime: z.date(),
 });
 
 export type Schema = z.infer<typeof schema>;
@@ -24,4 +25,5 @@ export const defaultValues: Schema = {
   languagesSpoken: [],
   gender: '',
   skills: [],
+  registrationDateAndTime: new Date(),
 }
