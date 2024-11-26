b9665704ec55676be84149bc704f3b3a77f3dff6
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-26 16:53:14 +0530

    installed formerEmploymentPeriod field to schema

--- src/Users/types/schema.ts
+++ src/Users/types/schema.ts
@@ -12,18 +12,22 @@ export const schema = z.object({
   states: z.array(z.string()).min(1).max(2),
   languagesSpoken: z.array(z.string()),
   gender: z.string().min(1),
-  skills: z.array(z.string()).max(2, { message: "You can only select up to 2 skills" }),
+  skills: z
+    .array(z.string())
+    .max(2, { message: "You can only select up to 2 skills" }),
   registrationDateAndTime: z.date(),
+  formerEmploymentPeriod: z.array(z.date()).max(2),
 });
 
 export type Schema = z.infer<typeof schema>;
 
 export const defaultValues: Schema = {
-  email: '',
-  name: '',
+  email: "",
+  name: "",
   states: [],
   languagesSpoken: [],
-  gender: '',
+  gender: "",
   skills: [],
   registrationDateAndTime: new Date(),
-}
+  formerEmploymentPeriod: [new Date(), new Date()],
+};
