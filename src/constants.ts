c69354d5b319848fa634c203eae158fee2f6e937
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-02 14:25:11 +0530

    made url a constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,9 @@
 export const patterns = {
   email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
 };
+
+/**
+ * The base URL of the REST API.
+ * This is used to construct the full URL of each query.
+ */
+export const url  = 'http://localhost:8080';
